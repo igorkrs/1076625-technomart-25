2896ea1dfa5d8434f1bc7bc255efe5b1e077efda
igorkrs <igorkrs@users.noreply.github.com>
2019-06-16 02:34:54 +0300

    +second slider

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var
   close_button = document.querySelector(".close"),
   cart_popup = document.querySelector(".added-to-cart");
 
+
 buy_buttons.forEach(function(el) {
   el.addEventListener("click", function(evt) {
     evt.preventDefault();
@@ -16,3 +17,19 @@ close_button.addEventListener("click", function(evt) {
   evt.preventDefault();
   cart_popup.classList.add("added-to-cart-hidden");
 });
+
+
+// var
+//   map_button = document.querySelector(".about-map"),
+//   close_button = document.querySelector(".map-close"),
+//   map_popup = document.querySelector(".map-block");
+
+// map_button.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   map_popup.classList.remove("map-block-hidden");
+// });
+
+// close_button.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   map_popup.classList.add("map-block-hidden");
+// });
