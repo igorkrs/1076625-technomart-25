115a7476e958938907517699ea44b9f362b19093
igorkrs <igorkrs@users.noreply.github.com>
2019-06-17 03:47:30 +0300

    js map

--- js/script.js
+++ js/script.js
@@ -3,33 +3,33 @@
 var
   buy_buttons = document.querySelectorAll(".main-item-buy"),
   close_button = document.querySelector(".close"),
-  cart_popup = document.querySelector(".added-to-cart");
+  cart_popup = document.querySelector(".added-to-cart"),
+  map_button = document.querySelector(".about-map"),
+  map_close = document.querySelector(".map-close"),
+  map_popup = document.querySelector(".map-block");
 
-
-buy_buttons.forEach(function(el) {
-  el.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    cart_popup.classList.remove("added-to-cart-hidden");
+if (close_button) {
+  buy_buttons.forEach(function(el) {
+    el.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      cart_popup.classList.remove("added-to-cart-hidden");
+    });
   });
-});
 
-close_button.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  cart_popup.classList.add("added-to-cart-hidden");
-});
+  close_button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    cart_popup.classList.add("added-to-cart-hidden");
+  });
+}
 
+if (map_close) {
+  map_button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    map_popup.classList.remove("map-block-hidden");
+  });
 
-// var
-//   map_button = document.querySelector(".about-map"),
-//   close_button = document.querySelector(".map-close"),
-//   map_popup = document.querySelector(".map-block");
-
-// map_button.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   map_popup.classList.remove("map-block-hidden");
-// });
-
-// close_button.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   map_popup.classList.add("map-block-hidden");
-// });
+  map_close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    map_popup.classList.add("map-block-hidden");
+  });
+}
