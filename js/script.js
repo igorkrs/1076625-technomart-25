a177013def18307cb14ab40fa31f2416de828cd4
igorkrs <igorkrs@users.noreply.github.com>
2019-06-27 01:44:33 +0300

    +arrows +jsFORM +fonts

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@ var
   cart_popup = document.querySelector(".added-to-cart"),
   map_button = document.querySelector(".about-map"),
   map_close = document.querySelector(".map-close"),
-  map_popup = document.querySelector(".map-block");
+  map_popup = document.querySelector(".map-block"),
+  form_button = document.querySelector(".about-link-popup"),
+  form_close = document.querySelector(".form-close"),
+  form_popup = document.querySelector(".popup");
+
 
 if (close_button) {
   buy_buttons.forEach(function(el) {
@@ -31,5 +35,18 @@ if (map_close) {
   map_close.addEventListener("click", function(evt) {
     evt.preventDefault();
     map_popup.classList.add("map-block-hidden");
+  });
+}
+
+
+if (form_close) {
+  form_button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    form_popup.classList.remove("popup-hidden");
+  });
+
+  form_close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    form_popup.classList.add("popup-hidden");
   });
 }
